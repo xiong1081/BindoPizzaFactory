e4f0059caba4579db73f8c1cb324dac960bc760d
半熟木屋 <164508906@qq.com>
2020-02-28 20:04:24 +0800

    Initial Commit

---
+++ BindoPizzaFactory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BindoPizzaFactory
+//
+//  Created by 李招雄 on 2020/2/28.
+//  Copyright © 2020 李招雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
